e2587975c6561a1902711b1bb221de9e1f027a50
RyotaKudo0816 <ryota.kudo.sys19@morijyobi.ac.jp>
2020-06-29 11:21:45 +0900

    edit 'update'

--- index.php
+++ index.php
@@ -113,7 +113,29 @@
                 </form>
             </div>
             <div class="tab-pane" id="update">
-                <p>smaple tab3</p>
+                <form class="mt-3" action="./update.php" method="POST">
+                        <div class="form-group row">
+                            <label for="id" class="col-sm-2 col-form-label">ID</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="id" name="id">
+                                </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="name" class="col-sm-2 col-form-label">Your Name</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="name" name="name">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="age" class="col-sm-2 col-form-label">Age</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="age" name="age">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Insert</button>
+                        </div>
+                    </form>
             </div>
             <div class="tab-pane" id="delete">
                 <p>smaple tab4</p>
